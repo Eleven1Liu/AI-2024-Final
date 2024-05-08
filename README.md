d33f90eab082298011a1dcb0f560476256b05e94
Eleven Liu <jiejyunliu@gmail.com>
2024-05-08 16:51:19 +0800

    Handle socket error without status response, add argparse & README

---
+++ README.md
@@ -0,0 +1,10 @@
+# AI Term Project
+
+## Collect Data
+
+```bash
+python download_data.py -t 300 -d data 
+```
+
+- `-d`: Path to the data directory.
+- `-t`: Time interval for fetching server.
--- download_data.py
+++ download_data.py
@@ -1,29 +1,49 @@
+import argparse
 import os
 import requests
 import time
 
 from datetime import datetime
+from socket import error as SocketError
 import pandas as pd
 
 
 URL = "https://tcgbusfs.blob.core.windows.net/dotapp/youbike/v2/youbike_immediate.json"
-INTERVAL = 300
-DATA_DIR = "data"
-os.makedirs(DATA_DIR, exist_ok=True)
 
 
-while True:
-    r = requests.get(URL)
-
-    # convert time
-    gmt_time = datetime.strptime(r.headers['Date'], "%a, %d %b %Y %H:%M:%S %Z")
-    formatted_time = gmt_time.strftime("%Y%m%d%H%M%S")
+def download_data(data_dir, interval):
+    try:
+        r = requests.get(URL)
+        gmt_time = datetime.strptime(r.headers['Date'], "%a, %d %b %Y %H:%M:%S %Z")
+        formatted_time = gmt_time.strftime("%Y%m%d%H%M%S")
+        
+        if r.status_code == 200:
+            df = pd.DataFrame(r.json())
+            df.to_csv(f"{data_dir}/{formatted_time}.csv", index=False)
+        else:
+            # log connection error (404, 500)
+            with open("error.log", "a") as f:
+                f.write("[{formatted_time}] Failed to retrieve data from the url.")
+        
+    except SocketError as e:
+        # socket error with out status code, pass and sleep for a while
+        print("Socket error! Sleep for 5 minutes.")
+        time.sleep(interval)
+        pass
+    time.sleep(interval)
     
-    if r.status_code == 200:
-        df = pd.DataFrame(r.json())
-        df.to_csv(f"{DATA_DIR}/{formatted_time}.csv", index=False)
-    else:
-        with open("error.log", "a") as f:
-            f.write("[{formatted_time}] Failed to retrieve data from the url.")
+
+def main():
     
-    time.sleep(INTERVAL)
+    parser = argparse.ArgumentParser(add_help=False)
+
+    # load params from config file
+    parser.add_argument("-d", "--data_dir", type=str, default="data", help="Path to the data directory.")
+    parser.add_argument("-t", "--interval", type=int, default=300, help="Time interval for fetching server.")
+    args, _ = parser.parse_known_args()
+    
+    while True:
+        os.makedirs(args.data_dir, exist_ok=True)
+        download_data(data_dir=args.data_dir, interval=args.interval)
+
+main()
